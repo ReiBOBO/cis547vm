ea78ef1ee5575720ad66b228174696188f0f8713
ReiBOBO <thm007@seas.upenn.edu>
2022-10-03 21:18:38 -0400

    update
    updated

--- lab3/src/Fuzzer.cpp
+++ lab3/src/Fuzzer.cpp
@@ -66,7 +66,8 @@ struct RunInfo {
  */
 // Collection of strings used to generate inputs
 std::vector<std::string> SeedInputs;
-
+// Add some mutated string that are not the best coverage one
+std::vector<std::string> Candidates; 
 // Variable to store coverage related information.
 std::vector<std::string> CoverageState;
 
@@ -77,14 +78,19 @@ std::vector<std::string> PrevCoverageState;
  * @brief Variable to keep track of some Mutation related state.
  * Feel free to change/ignore this if you want to.
  */
-int MutationState = 0;
-
+int MutationState = -1; //var that checks if current mutation causes improvement
+int MutationIndex = -1; //var that records current mutation index in MutationFns
+std::string BestInputSoFar=""; //store the best input so far(most coverage)
+std::string BestMutatedInputSoFar=""; //store the best mutated input
+int MaxCoverage = 0; //record max coverage number
 /**
  * @brief Variable to keep track of some state related to strategy selection.
  * Feel free to change/ignore this if you want to.
  */
-int StrategyState = 0;
-
+int StrategyState = -1; //var that describe the input index
+int StrategyIndex = -1; //store last good input index
+int InputCounter = 0;
+int MutationCounter = 0;
 /************************************************/
 /*    Implement your select input algorithm     */
 /************************************************/
@@ -101,16 +107,34 @@ int StrategyState = 0;
  * @return Pointer to a string.
  */
 std::string selectInput(RunInfo Info) {
-  int Index = 0;
-  return SeedInputs[Index];
+  int length = SeedInputs.size();
+  int Index = rand() % length;
+  if(InputCounter==0){
+    int luck = rand() % 3;
+    if(luck ==0) return SeedInputs[length-1];
+  }
+  if(InputCounter>10){
+    int luck = rand() % 3;
+    if(luck == 0 && Candidates.size()>1) 
+    return Candidates[rand() %Candidates.size()];
+  }
+  
+  if(StrategyState!=-1 && StrategyIndex!=-1){
+    while(Index == StrategyState){
+      Index = rand() % length;
+    }
+  }
+  StrategyIndex = Index;
+  int LetGodDecide =rand() %5;
+  if(LetGodDecide==0) return SeedInputs[Index];
+  if(LetGodDecide==1) return BestInputSoFar;
+  
+  return BestMutatedInputSoFar;
 }
 
 /*********************************************/
 /*       Implement mutation startegies       */
 /*********************************************/
-
-const char ALPHA[] = "abcdefghijklmnopqrstuvwxyz\n\0";
-const int LENGTH_ALPHA = sizeof(ALPHA);
 
 /**
  * Here we provide a two sample mutation functions
@@ -137,7 +161,9 @@ std::string mutationB(std::string Original) {
     return Original;
 
   int Index = rand() % Original.length();
-  return Original.insert(Index, 1, ALPHA[rand() % LENGTH_ALPHA]);
+  int randomASCII = rand() % 128;
+  char c = '\0' + randomASCII;
+  return Original.insert(Index, 1, c);
 }
 
 /**
@@ -148,15 +174,311 @@ std::string mutationB(std::string Original) {
  *
  * Get creative with your strategies.
  */
+/**
+* @brief Mutation Strategy that changes a random char of Original string
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutationC(std::string Original){
+  if (Original.length() <= 0)
+    return Original;
+  int randomASCII = rand() % 128;
+  char c = '\0' + randomASCII;
+  int randomIndex = rand() % Original.length();
+  Original[randomIndex] = c;
+  return Original;
+}
+/**
+*@brief double input size
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutationD(std::string Original){
+  if (Original.length() <= 0)
+    return Original;
+  return Original+Original;
+}
 
 /**
- * @brief Vector containing all the available mutation functions
- *
- * TODO: Update the definition to include any mutations you implement.
- * For example if you implement mutationC then update it to be:
- * std::vector<MutationFn *> MutationFns = {mutationA, mutationB, mutationC};
- */
-std::vector<MutationFn *> MutationFns = {mutationA, mutationB};
+*@brief half input size
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutationE(std::string Original){
+  if (Original.length() <= 0)
+    return Original;
+  return Original.substr(0,Original.size()/2);
+}
+
+
+/**
+*@brief force \n
+* @param Original Original input string.
+* @return "\n".
+*/
+std::string mutationF(std::string Original){
+  return "\n";
+}
+
+/**
+*@brief force NULL
+*/
+std::string mutationG(std::string Original){
+  return "\0";
+}
+
+/**
+*@brief Swap adjacent bytes
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutationH(std::string Original){
+  if (Original.length() <= 1)
+    return mutationC(Original);
+  int randomIndex = rand() % (Original.length()-1);
+  char c = Original[randomIndex];
+  Original[randomIndex] = Original[randomIndex+1];
+  Original[randomIndex+1]=c; 
+  return Original;
+}
+
+/**
+*@brief Remove a random byte
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutationI(std::string Original){
+  if (Original.length() <= 1)
+    return mutationC(Original);
+  int randomIndex = rand() % (Original.length());
+  Original.erase(randomIndex,1);
+  return Original;
+}
+
+/**
+*@brief Swap firstHalf with SecondHalf
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutationJ(std::string Original){
+  if (Original.length() <= 2)
+    return mutationC(Original);
+  int len = Original.length();
+  std::string firstHalf= Original.substr(0,len/2);
+  std::string secondHalf= Original.substr(len/2,len);
+  return secondHalf+firstHalf;
+}
+
+
+
+/**
+*@brief increase input size by random size and random characters
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutationN(std::string Original){
+  int randomIndex  = rand() % 300+1;
+ for(int i =0; i<randomIndex;i++){
+    int randomASCII = rand() % 128;
+    char c = '\0' + randomASCII;
+    Original.push_back(c);
+ }
+ return Original;
+}
+
+
+/**
+*@brief Cycle through and change every letter
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation1(std::string Original){
+  if (Original.length() <= 0)
+    return Original;
+ for(int i =0; i<Original.length();i++){
+    int randomASCII = rand() % 128;
+    char c = '\0' + randomASCII;
+    if(Original[i]!='\n')
+    Original[i] = c;
+ }
+ return Original;
+}
+
+/**
+*@brief insert '\n' to random positions
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation2(std::string Original){
+ int times = rand() % 5 +2;
+ for(int i =0; i<times; i++){
+  int randIndex = rand() % Original.length();
+
+    Original=Original.substr(0,randIndex)+"\n"+Original.substr(randIndex, Original.length());
+  
+ }
+ return Original;
+}
+
+/**
+*@brief decrease input size by random size and random characters
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation3(std::string Original){
+  int randomIndex  = rand() % Original.length();
+ for(int i =0; i<randomIndex;i++){
+    int randomASCII = rand() % 128;
+    char c = '\0' + randomASCII;
+    Original[i]=c;
+ }
+ return Original.substr(0,randomIndex);
+}
+
+/**
+*@brief randomly change characters not \n or \0 in given string
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation4(std::string Original){
+  int randomIndex  = rand() % Original.length();
+ for(int i =0; i<randomIndex;i++){
+    int randomASCII = rand() % 128;
+    char c = '\0' + randomASCII;
+    if(Original[i]!='\n' && Original[i]!='\0') Original[i]=c;
+    
+ }
+ return Original;
+}
+
+/**
+*@brief  change the whole string to a random character and skip \n and \0
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation5(std::string Original){ 
+  int randomASCII = rand() % 128;
+  char c = '\0' + randomASCII;
+ for(int i =0; i<Original.length();i++){
+   
+    if(Original[i]!='\n' && Original[i]!='\0') Original[i]=c;
+    
+ }
+ return Original;
+}
+
+/**
+*@brief  make all elements distinct
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation6(std::string Original){ 
+  if(Original.length()>128) return Original;
+  int startIdx = rand() % 128;
+  for(int i=0; i<Original.length();i++){
+    Original[i]='\0'+(i+startIdx)%128;
+  }
+  return Original;
+}
+
+/**
+*@brief  chang0e the whole string to a random character and not skip \n and \
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation7(std::string Original){ 
+  int randomASCII = rand() % 128;
+  char c = '\0' + randomASCII;
+ for(int i =0; i<Original.length();i++){
+   Original[i]=c;}
+  return Original;
+}
+
+/**
+*@brief  random sequence to idential ASCII letters
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation8(std::string Original){ 
+  if(Original.length()<=1) return Original;
+  int randomASCII = rand() % 128;
+  char c = '\0' + randomASCII;
+  int times = rand() % 5;
+  while(times>=0){
+    int startIdx = rand() % Original.length();
+    int length = rand() % Original.length();
+    while(length<Original.length()){
+      Original[length]=c;
+      length++;
+    }
+    times--;
+  }
+  return Original;
+}
+
+
+/**
+*@brief  change random number of chars to random ASCII char
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation9(std::string Original){ 
+  if(Original.length()<=1) return Original;
+  
+  int times = rand() % Original.length();
+  while(times>=0){
+    int randomASCII = rand() % 128;
+    char c = '\0' + randomASCII;
+    int idx = rand() % Original.length();
+    Original[idx]=c;
+    times--;
+  }
+  return Original;
+}
+
+/**
+*@brief  add \n to the end
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation10(std::string Original){ 
+  
+  return Original+"\n";
+}
+
+/**
+*@brief  random shuffle String 
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+std::string mutation11(std::string Original){ 
+  random_shuffle(Original.begin(),Original.end());
+  return Original+"\n";
+}
+
+
+/**
+*@brief decrease input size by random size and random characters
+* @param Original Original input string.
+* @return std::string mutated string.
+*/
+
+// std::vector<MutationFn *> MutationFnsWithoutRepeat = {mutationA, mutationB,mutationC,mutationD,mutationE,mutationF,mutationG,mutationH,mutationI,mutationJ,mutation1,mutationN,mutation2,mutation3,mutation4,mutation5};
+
+// /**
+// *@brief ran a random mutation three times
+// * @param Original Original input string.
+// * @return std::string mutated string.
+// */
+// std::string mutationK(std::string Original){
+//   if (Original.length() <= 2)
+//     return mutationC(Original);
+//   int len = MutationFnsWithoutRepeat.size();
+//   int idx = rand() %len;
+//   Original=MutationFnsWithoutRepeat[idx](Original);
+//   Original=MutationFnsWithoutRepeat[idx](Original);
+//   return Original;
+// }
 
 /**
  * @brief Select a mutation function to apply to the seed input.
@@ -169,9 +491,27 @@ std::vector<MutationFn *> MutationFns = {mutationA, mutationB};
  * @param RunInfo struct with information about the current run.
  * @returns a pointer to a MutationFn
  */
+/**
+ * @brief Vector containing all the available mutation functions
+ *
+ * TODO: Update the definition to include any mutations you implement.
+ * For example if you implement mutationC then update it to be:
+ * std::vector<MutationFn *> MutationFns = {mutationA, mutationB, mutationC};
+ */
+std::vector<MutationFn *> MutationFns = {mutationA, mutationB,mutationC,mutationD,mutationE,mutationF,mutationG,mutationH,mutationI,mutationJ,mutation1,mutationN,mutation2,mutation3,mutation4,mutation5,mutation6,mutation7,mutation8,mutation9,mutation10,mutation11};
 MutationFn *selectMutationFn(RunInfo &Info) {
   int Strat = rand() % MutationFns.size();
-
+  //  if(MutationCounter==0){
+  //   int meantToBe = rand() % 2;
+  //   if(meantToBe==1){return MutationFns[MutationFns.size()-1];}
+  // }
+  
+  if(MutationState!=-1 && MutationIndex!=-1){
+    while(MutationFns[Strat] == MutationFns[MutationState]){
+      Strat = rand() % MutationFns.size();
+    }
+  }
+  MutationIndex = Strat;
   return MutationFns[Strat];
 }
 
@@ -187,8 +527,9 @@ MutationFn *selectMutationFn(RunInfo &Info) {
 void feedBack(std::string &Target, RunInfo &Info) {
   std::vector<std::string> RawCoverageData;
   readCoverageFile(Target, RawCoverageData);
-
   PrevCoverageState = CoverageState;
+
+  //int currenmtCovLength = PrevCoverageState.size();
   CoverageState.clear();
 
   /**
@@ -205,10 +546,37 @@ void feedBack(std::string &Target, RunInfo &Info) {
    */
   CoverageState.assign(RawCoverageData.begin(),
                        RawCoverageData.end()); // No extra processing
+  if(CoverageState.size()>PrevCoverageState.size()){
+    Candidates.push_back(Info.MutatedInput);
+    if(CoverageState.size()>MaxCoverage){
+      MaxCoverage=CoverageState.size();
+      BestMutatedInputSoFar = Info.MutatedInput;
+      BestInputSoFar = Info.Input;
+    }
+    
+    //MutationFns.push_back(Info.Mutation);
+    MutationState =-1;
+    InputCounter=0;
+    MutationCounter=0;
+    
+  }
+  else{
+    if(Info.Passed){
+    int LetGodDecide = rand()%3;
+    MutationCounter++;
+    MutationState =MutationIndex;
+    //SeedInputs.push_back(Info.MutatedInput);
+  }
+  }
+ 
+  if(!Info.Passed){
+    BestMutatedInputSoFar = Info.MutatedInput;
+    BestInputSoFar = Info.Input;
+  }
 
 }
 
-int Freq = 1000;
+int Freq = 1;
 int Count = 0;
 int PassCount = 0;
 
@@ -223,6 +591,7 @@ bool test(std::string &Target, std::string &Input, std::string &OutDir) {
     fprintf(stderr, "%s not found\n", Target.c_str());
     exit(1);
   }
+  
   fprintf(stderr, "\e[A\rTried %d inputs, %d crashes found\n", Count,
           failureCount);
   if (ReturnCode == 0) {
--- lab4/delta_debugger/delta.py
+++ lab4/delta_debugger/delta.py
@@ -4,10 +4,10 @@ from typing import Tuple
 
 from delta_debugger import run_target
 
-EMPTY_STRING = b""
-
+EMPTY_STRING = b"" #global var to store the final input
 
 def delta_debug(target: str, input: bytes) -> bytes:
+    global EMPTY_STRING #define global
     """
     Delta-Debugging algorithm
 
@@ -21,4 +21,47 @@ def delta_debug(target: str, input: bytes) -> bytes:
     :param input: crashing input to be minimized
     :return: 1-minimal crashing input.
     """
-    return input
+    
+    EMPTY_STRING = input #initialize the global final result
+    helper(target,input,2) #call the helper function to find the one minimal
+    return EMPTY_STRING
+
+#helper function that runs the actual minimization algorithm
+def helper(target, input:bytes, n:int ):
+    global EMPTY_STRING #declare the global var
+    length = len(input) #get the length of the input
+    if(length<n): #base case for where the algorithm stops
+        if(run_target(target,b"")!=0): #we do want to cover the empty string case before we return
+            EMPTY_STRING=b""
+        return 
+    x:int = 0
+    # The idea is to recurse by cases:
+    # case 1. find delta fail then recurse on the delta with n=2
+    # case 2. find dabla fail then recurse on the dabla with n=n-1
+    # case 3. No fail found, then recurse on the input with n=n*2
+    while(x<length):
+        start:int = x
+        x=x+int(length/n)
+        delta=input[start:x]
+        dabla=input[0:start]+input[x:length]
+        passed=run_target(target,delta)
+        if(passed!=0): #failed good
+            if(len(EMPTY_STRING)>len(delta)):
+                EMPTY_STRING=delta #meaning we find a shorter input causes a failure
+            #case 1
+            helper(target,delta,2)
+            return #reduce the redundant runs, no need to run other branches
+        else:        
+            passed=run_target(target,dabla)
+            if(passed!=0):
+                if(len(EMPTY_STRING)>len(dabla)): 
+                    EMPTY_STRING=dabla #meaning we find a shorter input causes a failure
+                #case 2
+                helper(target,dabla,n-1)
+                return #reduce the redundant runs, no need to run other branches
+    #case 3
+    if(passed==0):
+        helper(target,input,n*2)  
+    
+    
+    
